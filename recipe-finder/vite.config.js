3a48ca152522c5c5288d8b6968e18ae439ad3efb
Junskywalkers <juntaka115@gmail.com>
2025-12-05 13:47:45 +0900

    fix: Vite config again

--- recipe-finder/vite.config.js
+++ recipe-finder/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-const repoName = '/antigravity/';
+const repoName = '/antigravity/recipe-finder/';
 
 export default defineConfig({
   plugins: [react()],
